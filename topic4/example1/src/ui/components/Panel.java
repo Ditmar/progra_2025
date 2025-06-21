4dda5665d57277207b6af4deb6ee10b858602c26
Ditmar <ditmarcastro@gmail.com>
2025-06-21 11:02:35 -0400

    feat: added grid and list

--- topic4/example1/src/ui/components/Panel.java
+++ topic4/example1/src/ui/components/Panel.java
@@ -1,15 +1,13 @@
 package ui.components;
 
-import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class Panel extends JPanel{
-    public Panel () {
+public class Panel extends JPanel {
+    public Panel() {
         super();
         this.setSize(new Dimension(100, 100));
-        this.setBackground(Color.PINK);
         this.setLayout(null);
     }
 }
